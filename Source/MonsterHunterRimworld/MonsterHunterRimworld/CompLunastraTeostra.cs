fa190e5b54220d69f2a684d57d6a1b23330f03cf
splincore <splincore@web.de>
2020-02-23 01:05:00 +0100

    changed Lunastra to also make heatwave instead of eclipse

--- Source/MonsterHunterRimworld/MonsterHunterRimworld/CompLunastraTeostra.cs
+++ Source/MonsterHunterRimworld/MonsterHunterRimworld/CompLunastraTeostra.cs
@@ -15,21 +15,11 @@ namespace MonsterHunterRimworld
             Map map = elderDragon.Map;
             if (map == null) return;
 
-            GameConditionManager gameConditionManager = map.GameConditionManager;
-            if (elderDragon.gender == Gender.Male)
-            {
-                if (gameConditionManager.ConditionIsActive(GameConditionDefOf.HeatWave)) return;
-                int duration = Mathf.RoundToInt(0.5f * 60000f); // lasts half a day after elder dragon dies
-                GameCondition cond = GameConditionMaker.MakeCondition(GameConditionDefOf.HeatWave, duration, 0);
-                gameConditionManager.RegisterCondition(cond);
-            }
-            else
-            {
-                if (gameConditionManager.ConditionIsActive(GameConditionDefOf.Eclipse)) return;
-                int duration = Mathf.RoundToInt(0.5f * 60000f); // lasts half a day after elder dragon dies
-                GameCondition cond = GameConditionMaker.MakeCondition(GameConditionDefOf.Eclipse, duration, 0);
-                gameConditionManager.RegisterCondition(cond);
-            }
+            GameConditionManager gameConditionManager = map.GameConditionManager;            
+            if (gameConditionManager.ConditionIsActive(GameConditionDefOf.HeatWave)) return;
+            int duration = Mathf.RoundToInt(0.5f * 60000f); // lasts half a day after elder dragon dies
+            GameCondition cond = GameConditionMaker.MakeCondition(GameConditionDefOf.HeatWave, duration, 0);
+            gameConditionManager.RegisterCondition(cond);
         }
     }
 }
