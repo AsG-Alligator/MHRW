739a67f5baf44d892cceb5b57010a9e2db9b0f75
Carny Senpai <splincore@web.de>
2021-02-21 13:11:02 +0100

    fixed various bugs

--- Source/MonsterHunterRimworld/MonsterHunterRimworld/Projectile_LightningBullet.cs
+++ Source/MonsterHunterRimworld/MonsterHunterRimworld/Projectile_LightningBullet.cs
@@ -3,7 +3,7 @@ using Verse;
 
 namespace MonsterHunterRimworld
 {
-    public class Projectile_LightningBullet: Bullet
+    public class Projectile_LightningBullet: Projectile_Explosive
     {
         public ThingDef_LightningBullet Def
         {
@@ -16,11 +16,8 @@ namespace MonsterHunterRimworld
         protected override void Impact(Thing hitThing)
         {
             IntVec3 strikeLocation = base.Position;
-
             Find.CurrentMap.weatherManager.eventHandler.AddEvent(new WeatherEvent_LightningStrike(this.Map, strikeLocation));
             base.Impact(hitThing);
         }
     }
-
-
 }
